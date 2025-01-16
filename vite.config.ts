ec12b38c140c23b169955854bfed0bbbfe5d9b98
wookyao <wook.yao@foxmail.com>
2025-01-16 14:39:11 +0800

    feat：init project

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,24 @@
+import path from "path";
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      "@/*": path.resolve(__dirname, "./src/*")
+    },
+  },
+  build: {
+    outDir: "dist",
+    minify: true,
+    // 打包后 去除 console
+    terserOptions: {
+      compress: {
+        drop_console: true,
+        drop_debugger: true
+      },
+    }
+  }
 })
